81300e38c72b43cf310451a9a822f666bffa8f88
Jarret Berman <jarretberman11@gmail.com>
2021-02-22 14:17:59 -0600

    tip pool serves.test.js

--- tip-pool/servers.test.js
+++ tip-pool/servers.test.js
@@ -11,7 +11,27 @@ describe("Servers test (with setup and tear-down)", function() {
     expect(allServers['server' + serverId].serverName).toEqual('Alice');
   });
 
+  it('it should not add a server if input is empty with submitServerInfo', function() {
+    serverNameInput.value = '';
+    submitServerInfo();
+
+    expect(Object.keys(allServers).length).toEqual(0)
+  })
+
+  it('it should update the servertable with a new server on updateServerTable()', function(){
+
+    submitServerInfo()
+    updateServerTable()
+
+    let newServer = document.querySelector("#serverTable tbody tr")
+    expect(newServer.childElementCount).toEqual(2)
+  })
+
   afterEach(function() {
     // teardown logic
+    serverTbody.innerHTML = ""
+    serverId = 0
+    allServers = {}
+
   });
 });
